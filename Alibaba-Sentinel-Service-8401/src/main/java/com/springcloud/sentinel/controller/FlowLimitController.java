56ecb47b9c995c70530631fab07887fd67fb5eef
Renaaa <ren812848195@163.com>
2021-02-19 00:56:41 +0800

    增加了sentinel的热点key的处理方式代码

--- Alibaba-Sentinel-Service-8401/src/main/java/com/springcloud/sentinel/controller/FlowLimitController.java
+++ Alibaba-Sentinel-Service-8401/src/main/java/com/springcloud/sentinel/controller/FlowLimitController.java
@@ -1,7 +1,14 @@
 package com.springcloud.sentinel.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author : renjiahui
@@ -10,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class FlowLimitController {
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("/testA")
     public String testA() {
@@ -21,4 +29,43 @@ public class FlowLimitController {
         return "----------testB";
     }
 
+    @GetMapping("/testD")
+    public String testD() {
+//        try {
+//            TimeUnit.SECONDS.sleep(1);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        logger.info("testD 测试RT");
+
+        logger.info("testD异常比例");
+        int age = 1 / 0;
+        return "----------testD";
+    }
+
+    @GetMapping("/testE")
+    public String testE() {
+        logger.info("testE 测试异常数");
+        int age = 10 / 0;
+        return "----------testE 测试异数";
+    }
+
+    /**
+     * SentinelResource
+     *      value中的值要唯一，可以不用是方法名，但要唯一
+     * @param p1
+     * @param p2
+     * @return
+     */
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        return "----------testHotKey";
+    }
+
+    public String deal_testHotKey(String p1, String p2, BlockException exception) {
+        //sentinel系统默认的提示：Blocked by Sentinel（flow limiting）
+        return "----------deal_testHotKey";
+    }
 }
